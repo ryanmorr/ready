2c3e133bb34ab136200d16cab78f3e324d8b6be0
Ryan Morr <rm.morr@gmail.com>
2015-03-14 01:34:52 -0400

    Improve unit tests

--- test/test-ready.js
+++ test/test-ready.js
@@ -3,35 +3,22 @@ describe('ready', function(){
 
     var expect = chai.expect;
 
-    it('should immediately invoke the callback if the element is currently ready', function(){
+    it('should immediately invoke the callback if the element is already available', function(){
         var spy = sinon.spy();
         ready('#container', spy);
         expect(spy.calledOnce).to.equal(true);
-        var container = spy.getCall(0).args[0];
-        expect(container.id).to.equal('container');
-        expect(spy.calledOn(container)).to.equal(true);
+        var element = spy.getCall(0).args[0];
+        expect(element.id).to.equal('container');
+        expect(document.body.contains(element)).to.equal(true);
+        expect(spy.calledOn(element)).to.equal(true);
     });
 
-    it('should immediately invoke the callback for every element matching the selector that is currently ready', function(){
-        var spy = sinon.spy();
-        ready('.foo', spy);
-        expect(spy.calledThrice).to.equal(true);
-        var div = spy.getCall(0).args[0];
-        expect(div.tagName.toLowerCase()).to.equal('div');
-        expect(div.className).to.equal('foo');
-        var span = spy.getCall(1).args[0];
-        expect(span.tagName.toLowerCase()).to.equal('span');
-        expect(span.className).to.equal('foo');
-        var section = spy.getCall(2).args[0];
-        expect(section.tagName.toLowerCase()).to.equal('section');
-        expect(section.className).to.equal('foo');
-    });
-
-    it('should invoke the callback when an element matching the selector is appended to the document dynamically', function(done){
-        var element;
-        var spy = sinon.spy(function(el){
+    it('should invoke the callback when an element is appended to the document dynamically', function(done){
+        var element,
+        spy = sinon.spy(function(added){
             expect(spy.calledOnce).to.equal(true);
-            expect(el).to.equal(element);
+            expect(added).to.equal(element);
+            expect(document.body.contains(added)).to.equal(true);
             done();
         });
         ready('.bar', spy);
@@ -42,23 +29,24 @@ describe('ready', function(){
         }, 200);
     });
 
-    it('should invoke the callback for every element matching the selector appended to the document dynamically', function(done){
-        var elements = [];
-        var spy = sinon.spy(function(el){
-            expect(el).to.equal(elements[spy.callCount - 1]);
+    it('should invoke the callback for multiple elements that match the selector', function(done){
+        var elements = [],
+        spy = sinon.spy(function(added){
+            expect(added).to.equal(elements[spy.callCount - 1]);
+            expect(document.body.contains(added)).to.equal(true);
             if(spy.calledThrice){
                 done();    
             }
         });
-        ready('.baz', spy);
+        ready('.baz.qux', spy);
         setTimeout(function(){
             var frag = document.createDocumentFragment();
-            for(var i=0; i < 4; i++){
-                var el = document.createElement('div');
-                el.className = 'baz';
-                frag.appendChild(el);
-                elements.push(el);
-            }
+            ['div', 'span', 'section'].forEach(function(tag){
+                var element = document.createElement(tag);
+                element.className = 'baz qux';
+                frag.appendChild(element);
+                elements.push(element);
+            });
             document.body.appendChild(frag);
         }, 200);
     });
