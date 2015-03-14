e2d43cb1a9ab92d240cb2d90b1b8666b73416e63
Ryan Morr <rm.morr@gmail.com>
2015-03-14 01:34:39 -0400

    Improve documentation

--- ready.js
+++ ready.js
@@ -14,26 +14,30 @@
 	observer;
 	
 	function ready(selector, fn){
-		// Append the specifics to an array so that it may be monitored
+		// Append the selector and function to an array so 
+		// that it may be monitored
 		listeners.push({
 			selector: selector,
 			fn: fn
 		});
+		// Create observer once
 		if(!observer){
-			// Use `MutationObserver` to watch for changes in the entire document
+			// Watch for changes in the document including
+			// all child nodes
 			observer = new MutationObserver(check);
 			observer.observe(doc.documentElement, {
 				childList: true,
 				subtree: true
 			});
-			// Disconnect the mutation observer on unload to avoid memory leaks
+			// disconnect on `unload` to avoid memory leaks
 			win.addEventListener('unload', observer.disconnect.bind(observer), false);
 		}
 		// Check elements currently in the DOM
 		check();
-	};
+	}
 		
-	// Check if any elements have been added to the DOM
+	// Check if any elements being watched have been 
+	// added to the DOM
 	function check(){
 		for(var i = 0, len = listeners.length, listener, elements; i < len; i++){
 			listener = listeners[i];
@@ -41,8 +45,8 @@
 			elements = doc.querySelectorAll(listener.selector);
 			for(var j = 0, jLen = elements.length, element; j < jLen; j++){
 				element = elements[j];
-				// Make sure the callback isn't invoked with the same element
-				// more than once
+				// Make sure the callback isn't invoked with the 
+				// same element more than once
 				if(!element.ready){
 					element.ready = true;
 					// Invoke the callback with the element
