be283ffbbc9cc74506690e1f50847d27ad094619
Ryan Morr <rm.morr@gmail.com>
2016-05-06 16:17:56 -0400

    Update documentation and formatting

--- src/ready.js
+++ src/ready.js
@@ -1,11 +1,4 @@
-/*
- * ready
- * Watch for when an element becomes available in the DOM
- * @param {String} selector
- * @param {Function} fn
- */
-
-(function(win){
+(function(win) {
     'use strict';
 
     var listeners = [], 
@@ -13,13 +6,57 @@
     MutationObserver = win.MutationObserver || win.WebKitMutationObserver,
     observer;
 
-    function ready(selector, fn){
+    /*
+     * Checks a selector for new matching
+     * elements and invokes the callback 
+     * if one is found
+     * 
+     * @param {String} selector
+     * @param {Function} fn
+     * @api private
+     */
+    function checkSelector(selector, fn) {
+        var elements = doc.querySelectorAll(selector), i = 0, len = elements.length, element;
+        for (; i < len; i++) {
+            element = elements[i];
+            // Make sure the callback isn't invoked with the 
+            // same element more than once
+            if (!element.ready) {
+                element.ready = true;
+                // Invoke the callback with the element
+                fn.call(element, element);
+            }
+        }
+    }
+
+    /*
+     * Check all selectors for new elements
+     * following a change in the DOM
+     * 
+     * @api private
+     */
+    function checkListeners() {
+        for (var i = 0, len = listeners.length, listener; i < len; i++) {
+            listener = listeners[i];
+            checkSelector(listener.selector, listener.fn);
+        }
+    }
+
+    /*
+     * Add a selector to watch for when a matching
+     * element becomes available in the DOM
+     * 
+     * @param {String} selector
+     * @param {Function} fn
+     * @api public
+     */
+    function ready(selector, fn) {
         // Store the selector and callback to be monitored
         listeners.push({
             selector: selector,
             fn: fn
         });
-        if(!observer){
+        if (!observer) {
             // Watch for changes in the document
             observer = new MutationObserver(checkListeners);
             observer.observe(doc.documentElement, {
@@ -29,29 +66,6 @@
         }
         // Check if the element is currently in the DOM
         checkSelector(selector, fn);
-    }
-
-    function checkListeners(){
-        // Check the DOM for elements matching a stored selector
-        for(var i = 0, len = listeners.length, listener; i < len; i++){
-            listener = listeners[i];
-            checkSelector(listener.selector, listener.fn);
-        }
-    }
-
-    function checkSelector(selector, fn){
-        // Query for elements matching the specified selector
-        var elements = doc.querySelectorAll(selector), i = 0, len = elements.length, element;
-        for(; i < len; i++){
-            element = elements[i];
-            // Make sure the callback isn't invoked with the 
-            // same element more than once
-            if(!element.ready){
-                element.ready = true;
-                // Invoke the callback with the element
-                fn.call(element, element);
-            }
-        }
     }
 
     // Expose `ready`
